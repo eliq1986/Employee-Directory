761c854ecca4327b94546f69d46936c01cb59f3b
Elijah Quesada <this_boy10@yahoo.com>
2019-01-06 07:28:15 -0800

    Placed modal text-content into function

--- js/app.js
+++ js/app.js
@@ -1,41 +1,46 @@
 "use strict";
 // API CALL
-fetch('https://randomuser.me/api/?results=12&inc=name,email,location,dob,picture,phone&nat=us')
-.then(response => response.json())
-.then(data => {
+fetch('https://randomuser.me/api/?results=12&inc=name,email,location,dob,picture,phone&nat=us').then(response => response.json()).then(data => {
 
-
-//DOM ELEMENTS
+  //DOM ELEMENTS
   const directoryContainer = document.getElementsByClassName("container")[0];
   const modal = document.querySelector(".modal");
   const modalContent = document.querySelector(".modal-content");
 
-
-
-// returns formatted DOB
+  // returns formatted DOB
   const formatBirthday = (data) => {
-    let dob = data.slice(0,10).split("-");
-    let day = dob.pop();
-    let month = dob.pop();
-    let year = dob.pop();
-    return  `${month}/${day}/${year}`;
-
+    let splitBirthday = data.slice(0, 10).split("-");
+    let day = splitBirthday.pop();
+    let month = splitBirthday.pop();
+    let year = splitBirthday.pop();
+    return `${month}/${day}/${year}`;
   }
 
-// appends input element
+  // appends input element
   let input = document.createElement("input");
   input.placeholder = "Search";
   document.querySelector("body").insertBefore(input, directoryContainer);
   const inputElement = document.querySelector("input");
 
-
-
- // holds JSON data
+  // holds JSON data
   let randomUsers = data.results;
 
+  //appends info to modal
+  function appendsEmployeeInfoToModal(userObj) {
+    document.querySelector(".modal-photo").src = userObj.photo;
+    document.querySelector(".modal-name").textContent = userObj.name;
+    document.querySelector(".modal-email").textContent = userObj.email;
+    document.querySelector(".modal-city").textContent = userObj.address.city;
 
-// maps through fetch api data and appends via innerHTML
-       directoryContainer.innerHTML = `
+    document.querySelector(".modal-phone").textContent = userObj.phone;
+    document.querySelector(".modal-address").textContent = userObj.address.street;
+    document.querySelector(".modal-birthday").textContent = `Birthday: ${formatBirthday(userObj.birthday)}`;
+    document.querySelector(".modal-border").style.display = "block";
+    modal.style.display = "block";
+  }
+
+  // maps through fetch api data and appends via innerHTML
+  directoryContainer.innerHTML = `
      ${randomUsers.map(randomUser => `
            <div class="box">
         <div>
@@ -53,65 +58,69 @@ fetch('https://randomuser.me/api/?results=12&inc=name,email,location,dob,picture
            </div>
            `).join("")}
        `
-       
-       //  listener fires when a box is clicked
-  directoryContainer.addEventListener("click", (event) => {
-     if (event.target.tagName === "P" || event.target.tagName === "IMG") {
 
-    document.querySelector(".modal-photo").src =   event.target.parentNode.parentNode.children[0].childNodes[1].attributes[2].value;
-    document.querySelector(".modal-name").textContent = event.target.parentNode.parentNode.children[1].children[0].textContent;
-    document.querySelector(".modal-email").textContent = event.target.parentNode.parentNode.children[1].children[1].textContent;
-    document.querySelector(".modal-city").textContent = event.target.parentNode.parentNode.children[1].children[2].textContent;
-    document.querySelector(".modal-border").style.display = "block";
-    document.querySelector(".modal-phone").textContent = event.target.parentNode.parentNode.children[1].children[4].textContent;
-    document.querySelector(".modal-address").textContent = event.target.parentNode.parentNode.children[1].children[5].textContent;
-    document.querySelector(".modal-birthday").textContent = `Birthday: ${formatBirthday(event.target.parentNode.parentNode.children[1].children[6].textContent)}`;
-    modal.style.display = "block";
-     }
-    else if (event.target.className === "box") {
-       document.querySelector(".modal-photo").src = event.target.children[0].childNodes[1].attributes[2].value;
-      document.querySelector(".modal-name").textContent = event.target.children[1].children[0].textContent;
-      document.querySelector(".modal-email").textContent = event.target.children[1].children[1].textContent;
-      document.querySelector(".modal-city").textContent = event.target.children[1].children[2].textContent;
-      document.querySelector(".modal-border").style.display = "block";
-      document.querySelector(".modal-phone").textContent = event.target.children[1].children[4].textContent;
-      document.querySelector(".modal-address").textContent = event.target.children[1].children[5].textContent;
-      document.querySelector(".modal-birthday").textContent = `Birthday: ${formatBirthday(event.target.children[1].children[6].textContent)}`;
-      modal.style.display = "block";
+  //  listener fires when a box is clicked
+  directoryContainer.addEventListener("click", event => {
+    let randomUser = {};
+    if (event.target.parentNode.parentNode.className === "box") {
 
-     }
+      randomUser = {
+        photo: event.target.parentNode.parentNode.children[0].childNodes[1].attributes[2].value,
+        name: event.target.parentNode.parentNode.children[1].children[0].textContent,
+        birthday: event.target.parentNode.parentNode.children[1].children[6].textContent,
+        email: event.target.parentNode.parentNode.children[1].children[1].textContent,
+        address: {
+          city: event.target.parentNode.parentNode.children[1].children[2].textContent,
+          street: event.target.parentNode.parentNode.children[1].children[5].textContent
+        },
+        phone: event.target.parentNode.parentNode.children[1].children[4].textContent
+      }
+      appendsEmployeeInfoToModal(randomUser);
+
+    } else if (event.target.className === "box") {
+      randomUser = {
+        photo: event.target.children[0].childNodes[1].attributes[2].value,
+        name: event.target.children[1].children[0].textContent,
+        birthday: event.target.children[1].children[6].textContent,
+        email: event.target.children[1].children[1].textContent,
+        address: {
+          city: event.target.children[1].children[2].textContent,
+          street: event.target.children[1].children[5].textContent
+        },
+        phone: event.target.children[1].children[4].textContent
+      }
+      appendsEmployeeInfoToModal(randomUser);
+    }
+
+  });
+
+  // closes modal
+  document.querySelector(".closeBtn").addEventListener("click", () => {
+    modalContent.classList.remove("modal-content");
+    modalContent.classList.add("modal-content-out");
+
+    setTimeout(() => {
+      modal.style.display = "none";
+      modalContent.classList.remove("modal-content-out");
+      modalContent.classList.add("modal-content");
+    }, 600);
 
   })
 
-// closes modal
-document.querySelector(".closeBtn").addEventListener("click",() => {
-   modalContent.classList.remove("modal-content");
-   modalContent.classList.add("modal-content-out");
+  //filters employees
+  inputElement.addEventListener("keyup", event => {
 
-setTimeout(()=> {
-  modal.style.display = "none";
-  modalContent.classList.remove("modal-content-out");
-  modalContent.classList.add("modal-content");
-}, 600);
+    let searchResult = inputElement.value.toUpperCase();
+    const employeeNames = [...document.getElementsByClassName("contact-name")];
 
-})
-
-//filters employees
-inputElement.addEventListener("keyup", (event)=> {
-
-let searchResult = inputElement.value.toUpperCase();
- const employeeNames = [...document.getElementsByClassName("contact-name")];
-
-   employeeNames.forEach(employee => {
-     const employeeNameCapitalized = employee.textContent.toUpperCase();
-      if (employeeNameCapitalized.indexOf(searchResult) > -1 ) {
-           employee.parentNode.parentNode.style.display = "";
-      }else {
-         employee.parentNode.parentNode.style.display = "none";
+    employeeNames.forEach(employee => {
+      const employeeNameCapitalized = employee.textContent.toUpperCase();
+      if (employeeNameCapitalized.indexOf(searchResult) > -1) {
+        employee.parentNode.parentNode.style.display = "";
+      } else {
+        employee.parentNode.parentNode.style.display = "none";
       }
-   })
-})
+    })
+  })
 
-
-})
-.catch(error => console.log("LOOKS LIKE THERE'S AN ERROR", error));
+}).catch(error => console.log("LOOKS LIKE THERE'S AN ERROR", error));
